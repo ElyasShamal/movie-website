783d9654667dd334999d5f85bd885d788d5716ed
Elyas <Elyas.shamal2@gmail.com>
2023-09-29 23:58:05 -0400

    footer left

--- movies/src/components/Home/Form.jsx
+++ movies/src/components/Home/Form.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Form() {
   return (
-    <form className="home-form">
-      <input type="text" placeholder="Email address"></input>
+    <form className="home-form" id="Form-one">
+      <input type="text" id="text" placeholder="Email address"></input>
       <button type="button">Get Started</button>
     </form>
   );
