4e08cb74c93bc4b95b8a8303f8dbb129e5fb37f2
Elyas <Elyas.shamal2@gmail.com>
2023-10-02 21:39:54 -0400

    update link

--- movies/src/components/Movies/Movie.js
+++ movies/src/components/Movies/Movie.js
@@ -3,28 +3,28 @@ import React, { useState, useEffect } from "react";
 function Movie() {
   const [movies, setMovies] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchMovie = () =>
-  //     fetch("https://api.sampleapis.com/movies/comedy").then((response) =>
-  //       response.json()
-  //     );
+  useEffect(() => {
+    const fetchMovie = () =>
+      fetch(
+        "https://phase-2-backend-json-server-template.onrender.com/Movies"
+      ).then((response) => response.json());
 
-  //   async function startFetching() {
-  //     const moviesData = await fetchMovie();
+    async function startFetching() {
+      const moviesData = await fetchMovie();
 
-  //     setMovies(moviesData);
-  //   }
-  //   startFetching();
-  // }, []);
+      setMovies(moviesData);
+    }
+    startFetching();
+  }, []);
 
   return (
     <section className="movies-container">
-      {/* {movies.map((movie) => (
+      {movies.map((movie) => (
         <div className="avaliable-movies" key={movie.id}>
-          <img src={movie.posterURL} alt={movie.title} />
+          <img src={movie.image} alt={movie.title} />
           <h2 style={{ color: "white" }}>{movie.title}</h2>
         </div>
-      ))} */}
+      ))}
     </section>
   );
 }
