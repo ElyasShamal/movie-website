8b405956fe72ff4bb1773bfe856ccd518d7ccd54
Elyas <Elyas.shamal2@gmail.com>
2023-10-03 01:20:55 -0400

    fetch data and add click and double click

--- movies/src/components/Movies/Movie.js
+++ movies/src/components/Movies/Movie.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import StarRating from "./StarRating";
 
 function Movie() {
   const [movies, setMovies] = useState([]);
+  const [hoverStates, setHoverStates] = useState({});
 
   useEffect(() => {
     const fetchMovie = () =>
@@ -11,18 +13,52 @@ function Movie() {
 
     async function startFetching() {
       const moviesData = await fetchMovie();
-
+      const initialClicksStates = {};
+      moviesData.forEach((movie) => {
+        initialClicksStates[movie.id] = false;
+      });
+      setHoverStates(initialClicksStates);
       setMovies(moviesData);
     }
     startFetching();
   }, []);
 
+  const handleDoubleClick = (movieId) => {
+    setHoverStates((prevState) => ({
+      ...prevState,
+      [movieId]: true,
+    }));
+  };
+
+  const handleClick = (movieId) => {
+    setHoverStates((prevState) => ({
+      ...prevState,
+      [movieId]: false,
+    }));
+  };
+
   return (
     <section className="movies-container">
       {movies.map((movie) => (
-        <div className="avaliable-movies" key={movie.id}>
-          <img src={movie.image} alt={movie.title} />
-          <h2 style={{ color: "white" }}>{movie.title}</h2>
+        <div className="available-movies" key={movie.id}>
+          <img
+            src={movie.image}
+            alt={movie.title}
+            onDoubleClick={() => handleDoubleClick(movie.id)}
+            onClick={() => handleClick(movie.id)}
+          />
+          <div style={{ position: "absolute" }}>
+            {hoverStates[movie.id] && (
+              <button className="hover-button">X</button>
+            )}
+          </div>
+          <div
+            className="titles"
+            style={{ color: "white", padding: "10px 0px 5px 10px" }}
+          >
+            <h3 style={{ color: "white" }}>{movie.title}</h3>
+            <span style={{ color: "orange" }}>{StarRating(movie.rating)} </span>
+          </div>
         </div>
       ))}
     </section>
---
+++ movies/src/components/Movies/StarRating.js
@@ -0,0 +1,14 @@
+function starRating(rating) {
+  let stars = "";
+  if (!rating) return "Not Rated";
+  for (let i = 0; i < rating; i++) {
+    stars += "★";
+  }
+  for (let i = 0; i < 5 - rating; i++) {
+    stars += "☆";
+  }
+
+  return stars;
+}
+
+export default starRating;
