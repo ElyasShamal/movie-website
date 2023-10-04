1ec7848695f2857c570ccda67caf4b9aec6fc21a
Elyas <Elyas.shamal2@gmail.com>
2023-10-03 20:52:24 -0400

    add style for form

--- movies/src/components/Login/Login.js
+++ movies/src/components/Login/Login.js
@@ -3,17 +3,28 @@ import React from "react";
 function Login() {
   return (
     <div className="login-form">
-      <form id="form-login">
+      <form id="login">
         <h2>Sign Up</h2>
         <label htmlFor="name">
           Name
           <input type="text" id="name" placeholder="Name" />
         </label>
-        <label htmlFor="Email">
+        <br></br>
+        <label htmlFor="email">
           Email
           <input type="text" id="email" placeholder="Email" />
         </label>
-        <button type="Submit">Sign Up</button>
+        <br></br>
+        <label htmlFor="password">
+          Passw
+          <input type="password" id="password" placeholder="Password" />
+        </label>
+        <br></br>
+        <div className="submit">
+          <button id="submit" type="Submit">
+            Sign Up
+          </button>
+        </div>
       </form>
     </div>
   );
